7689f5ed4e890471e61acbc24294e13f3e25e4cb
Pirmsho <d.pirmisashvili@gmail.com>
2022-01-16 17:46:53 +0100

    + admin page config && getting data

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts"),
+    path("posts/", views.posts, name="posts"),
     path('posts/<slug:slug>', views.post_detail, name="post-detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
+
+
+def get_date(post):
+    return post["date"]
 
 
 def starting_page(request):
-    return render(request, 'blog/index.html')
+    # get 3 latest posts
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+
+    return render(request, 'blog/index.html', {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
-    return render(request, 'blog/posts.html')
+    all_posts = Post.objects.all()
+    return render(request, 'blog/posts.html', {
+        "posts": all_posts
+    })
 
 
 def post_detail(request, slug):
-    return render(request, 'blog/post-detail.html')
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post-detail.html', {
+        "post": identified_post
+    })
